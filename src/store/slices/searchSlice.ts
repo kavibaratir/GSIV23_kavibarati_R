b54632cbca8fd07d7735d50271fc06c096f4fd9c
kavibaratir <32194416+kavibaratir@users.noreply.github.com>
2023-08-17 19:16:00 +0530

    Update searchSlice.ts

--- src/store/slices/searchSlice.ts
+++ src/store/slices/searchSlice.ts
@@ -2,21 +2,27 @@ import { createSlice } from "@reduxjs/toolkit";
 
 interface CounterState {
   value: string;
+  isDetailpageOpened:boolean
 }
 
 const initialState: CounterState = {
-  value: ""
+  value: "",
+  isDetailpageOpened:false
 };
 
 const counterSlice = createSlice({
   name: "searchData",
   initialState,
   reducers: {
+   
     searchOption: (state, action) => {
       state.value = action.payload;
+    },
+    displaySearch:(state, action)=>{
+      state.isDetailpageOpened=action.payload
     }
   }
 });
 
-export const { searchOption } = counterSlice.actions;
+export const { searchOption , displaySearch} = counterSlice.actions;
 export default counterSlice.reducer;
