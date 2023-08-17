f13b65ee1676358a7b9e893fbd377e66ee43f2c4
kavibaratir <32194416+kavibaratir@users.noreply.github.com>
2023-08-17 18:55:30 +0530

    Update Header.tsx

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -3,7 +3,8 @@ import { css } from "@emotion/css";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store";
-import { searchOption } from "../store/slices/searchSlice";
+import { searchOption ,displaySearch } from "../store/slices/searchSlice";
+import { useNavigate } from "react-router-dom";
 const headerCSS = {
   container: css({
     display: "flex",
@@ -18,17 +19,26 @@ const headerCSS = {
     width: 600
   })
 };
+
 const Header = () => {
+  const navigate = useNavigate();
   const [searchData, setSearchData] = useState("");
   const dispatch = useDispatch();
-
+  const displaySearchfield = useSelector(
+    (state: RootState) => state.searchData.isDetailpageOpened
+  );
+  const backToListPage=()=>{
+    dispatch(displaySearch(false))
+    navigate("/"); 
+  }
   return (
     <div className={headerCSS.container}>
+      { !displaySearchfield ?
       <InputGroup className={headerCSS.searchCSS}>
         <InputGroupText>
           <i className="bi bi-search"></i>
         </InputGroupText>
-
+    
         <Input
           placeholder="Search"
           data-testid="Searchbutton"
@@ -38,9 +48,10 @@ const Header = () => {
             dispatch(searchOption(e.target.value));
           }}
         />
-      </InputGroup>
+      </InputGroup>:<div><h4>Movie Details</h4></div>}
       <div className="px-2">
-        <i className="bi bi-house-door-fill"></i>
+        <i className="bi bi-house-door-fill"></i>{" "}
+        {displaySearchfield ? <i title="back" style={{cursor:"pointer"}}className="bi bi-arrow-90deg-left" onClick={backToListPage}></i>:""}
       </div>
     </div>
   );
